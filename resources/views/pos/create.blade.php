168fa131117c8b17cc44379dbe2c7a8ac7398f87
KarW010 <kartikostudy@gmail.com>
2026-09-15 16:20:39 +0700

    instal alpine.js

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -14,11 +14,9 @@ return this.cart.reduce((sum, item) => sum + item.price, 0);
 <div class="grid grid-cols-3 gap-4">
 @foreach ($products as $product)
 <div class="border rounded-md p-3 cursor-pointer"
-@click="addToCart({{ $product->id }}, '{{ $product->name }}', {{
-↪ $product->price }})">
+@click="addToCart({{ $product->id }}, '{{ $product->name }}', {{ $product->price }})">
 <p class="font-medium">{{ $product->name }}</p>
-<p class="text-sm text-slate-500">Rp {{
-↪ number_format($product->price) }}</p>
+<p class="text-sm text-slate-500">Rp {{number_format($product->price) }}</p>
 </div>
 @endforeach
 </div>
@@ -26,8 +24,7 @@ return this.cart.reduce((sum, item) => sum + item.price, 0);
 <template x-for="item in cart" :key="item.id">
 <p x-text="item.name + ' - Rp ' + item.price"></p>
 </template>
-<p class="font-semibold mt-2">Subtotal: Rp <span
-↪ x-text="subtotal()"></span></p>
+<p class="font-semibold mt-2">Subtotal: Rp <span x-text="subtotal()"></span></p>
 </div>
 </div>
 @endsection
